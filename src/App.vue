326f1f89f071f3635fd0252726271cfc6287d50c
Emily <lalonde0902@gmail.com>
2019-11-04 17:08:11 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <SearchForm />
+    <WordList />
+  </div>
+</template>
+
+<script>
+import SearchForm from './components/SearchForm/SearchForm'
+import WordList from './components/WordList/WordList'
+
+export default {
+  name: 'app',
+  components: {
+    SearchForm,
+    WordList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
