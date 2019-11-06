0d53b07da0ad2bee67af58b8a1b5f8aa96331614
Emily <lalonde0902@gmail.com>
2019-11-06 10:03:17 -0700

    Add snapshot tests for componenets

---
+++ src/App.test.js
@@ -0,0 +1,10 @@
+import { mount } from '@vue/test-utils';
+import App from './App.vue';
+
+describe('App', () => {
+
+  test('renders correctly', () => {
+    const wrapper = mount(App);
+    expect(wrapper.element).toMatchSnapshot();
+  })
+})
